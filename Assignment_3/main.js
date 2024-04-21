625049e4d158687b843205cf10809f11703f901e
SajidPanhwar <[mspanhwar1988@gmail.com]>
2024-04-21 15:37:13 +0500

    first commit

---
+++ Assignment_3/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// storing a famous qouts and it's author
+let qoute = "Nothing is impossible because I M a possible";
+let author = "Sajid Panhwar";
+// printing the message of qoutes
+console.log(`"${qoute}" - "${author}"`);
---
+++ Assignment_3/main.ts
@@ -0,0 +1,9 @@
+// storing a famous qouts and it's author
+
+let qoute:string = "Nothing is impossible because I M a possible";
+
+let author:string = "Sajid Panhwar";
+
+// printing the message of qoutes
+
+console.log(`"${qoute}" - "${author}"`);
